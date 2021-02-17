2cb80e4c5e1b2ffa56c447dee63fc3d3fe426086
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-02-17 00:37:05 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 include_once __DIR__ . '/../vendor/autoload.php';
 
-$parser = new aymanrb\UnstructuredTextParser\TextParser('/vendor/aymanrb/php-unstructured-text-parser/examples/templates');
+$parser = new aymanrb\UnstructuredTextParser\TextParser('/../vendor/aymanrb/php-unstructured-text-parser/examples/templates');
 
-$textToParse = preg_replace('/^[ \t]*[\r\n]+/m', '', strtolower(file_get_contents('/vendor/aymanrb/php-unstructured-text-parser/examples/test_txt_files/m_0.txt')));
+$textToParse = preg_replace('/^[ \t]*[\r\n]+/m', '', strtolower(file_get_contents('/../vendor/aymanrb/php-unstructured-text-parser/examples/test_txt_files/m_0.txt')));
 
 //performs brute force parsing against all available templates, returns first match successful parsing
 $parseResults = $parser->parseText($textToParse);
