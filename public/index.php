3d68ede75b56e24dd330e948ea4b6b73d65f3c9a
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-02-25 09:58:00 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -66,7 +66,7 @@ function funcaoRegys($mensagem){
 }
 
 function funcaoWR($mensagem){
-	$parser = new aymanrb\UnstructuredTextParser\TextParser('../path/to/templatesDirectory');
+	$parser = new aymanrb\UnstructuredTextParser\TextParser('../vendor/aymanrb/php-unstructured-text-parser/examples/templates');
 	//Mudar para diretório referente ao GitHub!!!!
 	$textToParse = preg_replace("/^[ \t]*[\r\n]+/m", "", strtolower($mensagem));
 	$parseResults = $parser->parseText($textToParse, true)->getParsedRawData();
