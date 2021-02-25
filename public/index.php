f57ed8675991809a52e327fc508518a3855b78ed
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-02-25 01:00:16 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,9 +5,9 @@ $requisicao = array(
 	"messages"=>array(
 			0=>array(
 				"id"=>"false_558399711150@c.us_3EB02CA3A26371A62F72",
-				"body"=>"Pernambucano
-Santa Cruz x vitória das tabocas
-Over 2,5 @1,80
+				"body"=>"Série A 
+Bragantino ML
+Min 1,61
 1u",
 				"fromMe"=>0,
 				"self"=>0,
